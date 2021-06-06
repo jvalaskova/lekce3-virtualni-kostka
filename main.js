f729833a10b4ed9f4beaa412b9bd7e70c3dcae09
Jana Valášková <jancav@seznam.cz>
2021-06-06 17:26:38 +0200

    kostka--final

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // tady je místo pro náš program
+
+
+
+function hrej() {
+    let kostka = document.querySelector("#kostka");
+    let zprava = document.querySelector("#zprava");
+    let hod = Math.floor(Math.random() * 6 + 1); 
+    kostka.src = "obrazky/" + hod + ".png";
+
+    if (hod === 6) {
+        zprava.textContent = "Vyhrál jsi! Hodil jsi číslo 6."
+    } else {
+        zprava.textContent = "Hodil jsi číslo" + hod + ", zkus to znovu.";
+    }
+
+}
+
